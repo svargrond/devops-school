ef92fa8fb53b07dbb91739797afea22f90090595
svargrond <86289629+svargrond@users.noreply.github.com>
2021-06-22 14:42:20 +0300

    Testing env variable

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
+print(os.environ['USER_VALUE'])
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp}')
+    f.write(f'# {timestamp} {USER_VALUE}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
