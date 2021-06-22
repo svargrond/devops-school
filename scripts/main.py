db8b5ed53e40ff42cfac10e39211391f80a33444
svargrond <86289629+svargrond@users.noreply.github.com>
2021-06-22 15:58:13 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -2,11 +2,12 @@ from datetime import datetime
 import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
-print(os.getenv('USER_VALUE'))
+
+uservalue = os.getenv('USER_VALUE')
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp}')
+    f.write(f'# {timestamp} {uservalue}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
