bec1270a5b9151e74b3076b41e632974870ad31f
svargrond <86289629+svargrond@users.noreply.github.com>
2021-06-22 15:45:08 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
+print(os.getenv('IMPORT_USER_VALUE'))
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
