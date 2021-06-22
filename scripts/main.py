f6e990e17b58b105f878e319ea7fb2f8c5452d25
svargrond <86289629+svargrond@users.noreply.github.com>
2021-06-22 15:00:20 +0300

    Added INPUT_ to env var

--- scripts/main.py
+++ scripts/main.py
@@ -3,10 +3,10 @@ import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
-print(os.environ['USER_VALUE'])
+print(os.getenv['INPUT_USER_VALUE'])
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp} {USER_VALUE}')
+    f.write(f'# {timestamp} {INPUT_USER_VALUE}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
